d76be47d72a7e81ab10cf277596165e181e08616
withoutaunt <cinnalb@gmail.com>
2022-05-17 01:07:46 -0300

    commit2

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -6,7 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista.component.scss']
 })
 export class ListaComponent implements OnInit {
-
+  propertyToCheck=1;
+  boolToCheck=true;
+  estudiantes=[
+    {
+      nombre: 'Camila Gonzales',
+      nota:6,
+      curso: 'Angular'
+    },
+    {
+      nombre: 'Lucas Paz',
+      nota:8,
+      curso: 'React'
+    },
+    {
+      nombre: 'Jazmin Paez',
+      nota:3,
+      curso: 'React'
+    },
+    {
+      nombre: 'Tomas Perez',
+      nota:10,
+      curso: 'Angular'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
