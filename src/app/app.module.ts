1b430f4a81e39fcb27ad5bf766cb3ece3dbaa048
withoutaunt <cinnalb@gmail.com>
2022-05-23 21:42:38 -0300

    forms1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaComponent } from './lista/lista.component';
+import { Formulario1Component } from './formulario1/formulario1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaComponent
+    ListaComponent,
+    Formulario1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/formulario1/formulario1.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Formulario1Component } from './formulario1.component';
+
+describe('Formulario1Component', () => {
+  let component: Formulario1Component;
+  let fixture: ComponentFixture<Formulario1Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Formulario1Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Formulario1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
